2406faa169d580595631f98936b07b3fc14508b3
ibozic90 <ibozic90@gmail.com>
2022-04-09 11:50:12 +0300

    Добавьте файлы проекта.

---
+++ WordPad_WF/Controls/CustomTabControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WordPad_WF.Controls
+{
+    class CustomTabControl : TabControl
+    {
+
+    }
+}
---
+++ WordPad_WF/Controls/CustomTetxBox.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WordPad_WF.Controls
+{
+    class CustomTetxBox : RichTextBox
+    {
+
+    }
+}
